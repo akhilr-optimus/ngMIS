a1795f7e8b48aaaf8e94ed77978bbe9c1a484ef5
akhilr-optimus <akhil.rangi@optimusinfo.com>
2016-05-10 19:03:13 +0530

    mis6

--- MIS/App_NG/controllers/projectDetailsCtrl.js
+++ MIS/App_NG/controllers/projectDetailsCtrl.js
@@ -2,7 +2,7 @@
 (function () {
     var app = angular.module('dashMIS');
 
-    var projectDetailsCtrl = function ($scope, dataService,$location) {
+    var projectDetailsCtrl = function ($scope, dataService, $location) {
 
         $scope.onEdit = function (data) {
             dataService.setSelectedProject(data);
@@ -11,13 +11,14 @@
 
         $scope.onEditModal = function (data) {
             dataService.setSelectedProject(data);
-            $location.url("/ProjectEdit");
+
+            //$location.url("/ProjectEdit");
         };
 
         $scope.getData = function () {
             $scope.projectsData = dataService.getAllProjects();
         };
-                
+
         $scope.getData();
     };
 
